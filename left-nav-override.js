15dabb77a289bbcb934f9b0ee8c478b4ebcb9af1
sqlsolver <rmaxwellm@cfgo.com>
2018-12-03 10:41:46 -0800

    Created nested functions, URL is being captured in local storage.

--- left-nav-override.js
+++ left-nav-override.js
@@ -1,13 +1,37 @@
 console.log("QuickLaunch navigation dropdowns provisioning.")
 _spBodyOnLoadFunctionNames.push("skinClassicQL");
- console.log("QuickLaunch navigation dropdowns enabled.")
+ console.log("QuickLaunch override loaded.")
 
 function skinClassicQL(){
-  $("div[id$=_V4QuickLaunchMenu]").addClass("base");
-  $("div[id^=zz][id$=_V4QuickLaunchMenu] li").addClass("baseBorder");
-  $("div[id^=zz][id$=_V4QuickLaunchMenu] span").addClass("baseText");
-  $("div[id^=zz][id$=_V4QuickLaunchMenu] ul li").has('ul').addClass('Office365Icon');
-  $("[id^=zz][id$=_RootAspMenu] li").children('ul').hide();
+  function basicStyle() {
+    $("div[id$=_V4QuickLaunchMenu]").addClass("base");
+    $("div[id^=zz][id$=_V4QuickLaunchMenu] li").addClass("baseBorder");
+    $("div[id^=zz][id$=_V4QuickLaunchMenu] span").addClass("baseText");
+    $("div[id^=zz][id$=_V4QuickLaunchMenu] ul li").has('ul').addClass('Office365Icon');
+    $("[id^=zz][id$=_RootAspMenu] li").children('ul').hide();
+  }
+
+  //nested function toggles menu
+  function openClose(e) {
+    $(this).children('ul').slideToggle(300).removeClass("baseText").addClass("subMenu subMenuText");
+    $(this).siblings('li').children('ul').hide();
+    console.log(e + "You activated the toggle.");
+  }
+ 
+  //nested function stores target URL when navigating to new page, and restores state after navigation
+  function storeUrl(e) {
+    //var anchor = $(this);
+    console.log(this.getAttribute("href") + " Using plain this.currentTarget.getattribute..");
+    if (e) {
+      localStorage.setItem('selectedItem', this.getAttribute("href"));
+      console.log("Local storage value for selectedItem is: " + window.localStorage.getItem('selectedItem'));
+      console.log("Congratulations! You triggered the click event to populate the local storage");
+    }
+  }
+
+basicStyle();
+$("[id^=zz][id$=_RootAspMenu] li").on("click", openClose);
+$("[id^=zz][id$=_RootAspMenu]").on("click", "a", storeUrl);
 
   /* for the if stmt something like if window.location === to selectedItem
   if (window.localStorage.getItem('selectedItem')) {
@@ -17,20 +41,7 @@ function skinClassicQL(){
   else {
     $("[id^=zz][id$=_RootAspMenu] li ul").hide();
   }*/ 
-  $("[id^=zz][id$=_RootAspMenu] li").click(function(e) {
-    $(this).children('ul').slideToggle(300).removeClass("baseText").addClass("subMenu subMenuText");
-    $(this).siblings('li').children('ul').hide();
-    console.log(e + "You activated the toggle.");
-  });
 
-  //Removed classes since they don't have href attr [selected][ms-quicklaunch-dropNode][ms-core-listMenu-selected] 
-
-    //Because the link is dynamic bind to the document? $(document).on(click, "a", function(){} ); - except don't want to bind to clicks anywhere on the page - only the QL so 
-    $("div[id$=_V4QuickLaunchMenu]").on($("a").click(function(e) {
-    var targetLink2 = $(this.currentTarget).getAttribute("href");
-    console.log(targetLink2 + " is the href attribute of the anchor tag.");
-    window.localStorage.setItem('selectedItem', $(this).targetLink2);
-  }));
-
+  //Hide the ability to modify QL navigation
   $("span[id^=zz][id$=_V4QuickLaunchMenu_NavMenu_Edit]").hide();
   }
