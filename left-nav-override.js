19f77740e32701b951f2f3afa3c880e1d4c44291
sqlsolver <rmaxwellm@cfgo.com>
2018-12-04 18:10:46 -0800

    Rearranged function order.

--- left-nav-override.js
+++ left-nav-override.js
@@ -3,45 +3,48 @@ _spBodyOnLoadFunctionNames.push("skinClassicQL");
  console.log("QuickLaunch override loaded.")
 
 function skinClassicQL(){
+  //start out with the skinned menu
   function basicStyle() {
-    $("div[id$=_V4QuickLaunchMenu]").addClass("base");
+    $("div[id^=zz][id$=_V4QuickLaunchMenu]").addClass("base");
     $("div[id^=zz][id$=_V4QuickLaunchMenu] li").addClass("baseBorder");
     $("div[id^=zz][id$=_V4QuickLaunchMenu] span").addClass("baseText");
     $("div[id^=zz][id$=_V4QuickLaunchMenu] ul li").has('ul').addClass('Office365Icon');
     $("[id^=zz][id$=_RootAspMenu] li").children('ul').hide();
   }
 
-  //nested function toggles menu
+     //function toggles menu
   function openClose(e) {
     $(this).children('ul').slideToggle(300).removeClass("baseText").addClass("subMenu subMenuText");
     $(this).siblings('li').children('ul').hide();
     console.log(e + "You activated the toggle.");
   }
- 
-  //nested function stores target URL when navigating to new page, and restores state after navigation
-  function storeUrl(e) {
-    //var anchor = $(this);
-    console.log(this.getAttribute("href") + " Using plain this.currentTarget.getattribute..");
-    if (e) {
-      localStorage.setItem('selectedItem', this.getAttribute("href"));
-      console.log("Local storage value for selectedItem is: " + window.localStorage.getItem('selectedItem'));
-      console.log("Congratulations! You triggered the click event to populate the local storage");
-    }
+
+//function stores target URL when navigating to new page, and restores state after navigation
+function storeUrl(e) {
+  console.log(this.getAttribute("href") + " Logging the current navigation URL.");
+  if (e) {
+    localStorage.setItem('selectedItem', this.getAttribute("href"));
+    console.log("Local storage value for selectedItem is: " + localStorage.getItem('selectedItem'));
   }
+}
+ 
+ //if localStorage is !null, open the drawer containing its anchor plus highlight the anchor
+ function selectedStyle() {
+  if(window.location.href.indexOf("localStorage.getItem('selectedItem')") > -1) {
+    $("[id^=zz][id$=_RootAspMenu] li").children('ul').slideToggle(300).removeClass("baseText").addClass("subMenu subMenuText"); 
+    $("[id^=zz][id$=_RootAspMenu] li").siblings('li').children('ul').hide();
+    $("[id^=zz][id$=_RootAspMenu] span").addClass("subMenuSelected"); 
+  }
+  else {
+    console.log("The target value is empty.");
+  }  
+}  
 
 basicStyle();
+selectedStyle();
 $("[id^=zz][id$=_RootAspMenu] li").on("click", openClose);
 $("[id^=zz][id$=_RootAspMenu]").on("click", "a", storeUrl);
 
-  /* for the if stmt something like if window.location === to selectedItem
-  if (window.localStorage.getItem('selectedItem')) {
-    $("[id^=zz][id$=_RootAspMenu] li ul").children.removeClass("baseText").addClass("subMenu subMenuText").show();
-    $("[id^=zz][id$=_RootAspMenu] li ul").siblings('li').children('ul').hide(); 
-  }
-  else {
-    $("[id^=zz][id$=_RootAspMenu] li ul").hide();
-  }*/ 
-
   //Hide the ability to modify QL navigation
-  $("span[id^=zz][id$=_V4QuickLaunchMenu_NavMenu_Edit]").hide();
-  }
+$("span[id^=zz][id$=_V4QuickLaunchMenu_NavMenu_Edit]").hide();
+}
